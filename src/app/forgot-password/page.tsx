ecc98325eddb7a7cae2c7c698c6f620dc10b4879
MagroOwner <Alexsharber1@gmail.com>
2026-09-05 21:21:55 -0400

    Add password reset page

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+import { ForgotPasswordForm } from "@/components/forms/ForgotPasswordForm";
+
+export const dynamic = "force-dynamic";
+
+export default function ForgotPasswordPage() {
+  return <ForgotPasswordForm />;
+}
